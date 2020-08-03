a17e2b28fbf7ed3105b6aed1c7fdf4eb2ca08bbf
Bice <59291964+certifiedbice@users.noreply.github.com>
2020-08-03 17:22:13 -0600

    updates for heroku deployment

--- src/config.js
+++ src/config.js
@@ -1,6 +1,8 @@
 module.exports={
     PORT:process.env.PORT||8000,
     NODE_ENV:process.env.NODE_ENV||'development',
-    DB_URL:process.env.DB_URL||'postgresql://noteful:noteful@localhost/noteful',
-    TEST_DB_URL:process.env.TEST_DB_URL||'postgresql://noteful:noteful@localhost/noteful-test',
+    //DB_URL:process.env.DB_URL||'postgresql://noteful:noteful@localhost/noteful',
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://noteful:noteful@localhost/noteful',
+    //TEST_DB_URL:process.env.TEST_DB_URL||'postgresql://noteful:noteful@localhost/noteful-test',
+    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://noteful:noteful@localhost/noteful-test',
 }
